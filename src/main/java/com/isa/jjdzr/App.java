31bf33a81bbe1faa8d928d4e50da90293ee2bf79
Piotr Olszewski <serviceskpa@gmail.com>
2023-01-23 18:17:28 +0100

    Call changed to Team name

--- src/main/java/com/isa/jjdzr/App.java
+++ src/main/java/com/isa/jjdzr/App.java
@@ -1,13 +1,9 @@
 package com.isa.jjdzr;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Code Crunchers");
     }
 }
