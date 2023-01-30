205262c7ed0915a4028ac5b4e4063d8adc14cc04
Jacek Lewczuk <jlewczuk@onet.eu>
2023-01-30 17:11:04 +0100

    App scheme

---
+++ skirch-console/src/main/java/com/isa/jjdzr/App.java
@@ -0,0 +1,13 @@
+package com.isa.jjdzr;
+
+/**
+ * Code crunchers
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Code crunchers" );
+    }
+}
