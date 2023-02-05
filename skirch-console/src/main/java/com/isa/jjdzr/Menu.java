57fdfb1a91b8a9e783ea0fee66a0a9e0b7c71268
Piotr Olszewski <serviceskpa@gmail.com>
2023-02-05 20:15:55 +0100

    Menu class added

---
+++ skirch-console/src/main/java/com/isa/jjdzr/Menu.java
@@ -0,0 +1,26 @@
+package com.isa.jjdzr;
+
+
+//dołożyć logikę biznesową pod działanie menu do podmianki IO
+// DataTranferObject
+
+import java.util.Scanner;
+
+public class Menu {
+
+    public void menu() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int direction = scanner.nextInt();
+        switch (direction) {
+            case 1 -> System.out.println("1");
+            case 2 -> System.out.println("2");
+            case 3 -> System.out.println("3");
+            case 4 -> System.out.println("4");
+            case 5 -> System.out.println("5");
+            default -> System.out.println("0!");
+        }
+    }
+}
+
