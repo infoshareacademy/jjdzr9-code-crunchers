cabe21c81b1b2ac9d09541cce3a6926377a0e8b9
Karolina Smentoch <karolinasmentoch@gmail.com>
2023-02-10 10:07:44 +0100

    Add Json deserialized - Status is ignored

---
+++ skirch-console/src/main/java/com/isa/jjdzr/deserializer/Lifts.java
@@ -0,0 +1,32 @@
+package com.isa.jjdzr.deserializer;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(value = { "status" })
+
+public class Lifts {
+    Status status;
+    Stats stats;
+    public Lifts() {
+    }
+    public Status getStatus() {
+        return status;
+    }
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+    public Stats getStats() {
+        return stats;
+    }
+    public void setStats(Stats stats) {
+        this.stats = stats;
+    }
+
+    @Override
+    public String toString() {
+        return "Lifts{" +
+                "status=" + status +
+                ", stats=" + stats +
+                '}';
+    }
+}
---
+++ skirch-console/src/main/java/com/isa/jjdzr/deserializer/Resort.java
@@ -0,0 +1,12 @@
+package com.isa.jjdzr.deserializer;
+
+public class Resort {
+    Data data;
+
+    public Data getData() {
+        return data;
+    }
+    public void setData(Data data) {
+        this.data = data;
+    }
+}
