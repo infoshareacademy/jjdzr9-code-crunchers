49065f665e471573a88a55935af2429aad10b2cb
Karolina Smentoch <karolinasmentoch@gmail.com>
2023-02-13 23:27:31 +0100

    Add SearchEngine class

--- skirch-console/src/main/java/com/isa/jjdzr/App.java
+++ skirch-console/src/main/java/com/isa/jjdzr/App.java
@@ -1,15 +1,24 @@
 package com.isa.jjdzr;
 
 import com.isa.jjdzr.deserializer.JsonDeserializer;
+import com.isa.jjdzr.deserializer.Resort;
 
 import java.io.IOException;
+import java.util.List;
 
 public class App 
 {
     public static void main( String[] args ) throws IOException {
         System.out.println( "Code crunchers" );
-        JsonDeserializer deserializer = new JsonDeserializer();
-        deserializer.deserialize();
+        System.out.println();
+
+        List<Resort> resorts = JsonDeserializer.deserialize();
+        SearchEngine sE = new SearchEngine();
+
+        // wyszukiwanie po nazwie, w miejsce stringa wskoczy to co poda użytkownik
+        sE.searchByName("Wieżyca Koszałkowo", resorts);
+        // wyszukiwanie po województwie, w miejsce stringa wskoczy to co poda użytkownik
+        sE.searchByRegion("śląskie", resorts);
 
 
 
