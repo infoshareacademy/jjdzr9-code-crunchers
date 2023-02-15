44881ec9f001368c480ab3c2d723de8e10ecd1db
Piotr Olszewski <serviceskpa@gmail.com>
2023-02-15 12:47:03 +0100

    Choices extended

--- skirch-console/src/main/java/com/isa/jjdzr/Menu.java
+++ skirch-console/src/main/java/com/isa/jjdzr/Menu.java
@@ -1,14 +1,18 @@
 package com.isa.jjdzr;
 
 
-//dołożyć logikę biznesową pod działanie menu do podmianki IO
-// DataTranferObject
-
+import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
 
+    private final List<String> options = List.of("1. Wyszukaj po nazwie", "2. Wyszukaj po koordynatach",
+            "3. Wyszukaj po kraju", "4. Wyszukaj po regionie", "5. Zaloguj", "6. Utwórz użytkownika");
+
     public void menu() {
+        System.out.println(" ------- WITAMY W WYSZUKIWARCE DLA WIELBICIELI BIAŁEGO SZALEŃSTWA!!! ------- ");
+        sleep();
+        displayOptions();
 
         Scanner scanner = new Scanner(System.in);
 
@@ -20,6 +24,18 @@ public class Menu {
             case 4 -> System.out.println("4");
             case 5 -> System.out.println("5");
             default -> System.out.println("0!");
+        }
+    }
+
+    public void displayOptions() {
+        options.forEach(options -> System.out.println(options));
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 }
