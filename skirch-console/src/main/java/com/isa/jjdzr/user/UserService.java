7d760e9c59ee491ca491da935a8373f65693f373
Maciej <m-bohn@wp.pl>
2023-03-12 21:07:23 +0100

    Dodałem klasy dodające użytkownika.

--- skirch-console/src/main/java/com/isa/jjdzr/user/UserService.java
+++ skirch-console/src/main/java/com/isa/jjdzr/user/UserService.java
@@ -1,7 +1,19 @@
 package com.isa.jjdzr.user;
 
-public class UserService {
-    public void saveUser (User user) {
+import java.util.ArrayList;
+import java.util.List;
 
+public class UserService {
+    private final List<User> userList = new ArrayList<>();
+    public UserService() {
+        User newUser = new User();
+        saveUser(newUser);
+    }
+    public void saveUser(User user) {
+        userList.add(user);
+        System.out.println("User " + user.getUsername() + " saved.");
+    }
+    public List<User> getUserList() {
+        return userList;
     }
 }
