794d5948b26e5e24279aae880f4eaba89b1fb3d2
Karolina Smentoch <karolinasmentoch@gmail.com>
2023-04-14 17:43:52 +0200

    Fix bug with test in skirch-web

--- skirch-web/src/test/java/com/isa/jjdzr/SkirchWebApplicationTests.java
+++ skirch-web/src/test/java/com/isa/jjdzr/SkirchWebApplicationTests.java
@@ -3,7 +3,7 @@ package com.isa.jjdzr;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes=com.isa.jjdzr.SkirchWebApplication.class)
 class SkirchWebApplicationTests {
 
     @Test
