6089bc3e720ce551dca5b63842034c292f9a4df6
Piotr Olszewski <serviceskpa@gmail.com>
2023-04-20 17:15:36 +0200

    Endpoints added. HTML altered

--- skirch-web/src/main/java/com/isa/jjdzr/SkirchWebApplication.java
+++ skirch-web/src/main/java/com/isa/jjdzr/SkirchWebApplication.java
@@ -1,13 +1,17 @@
 package com.isa.jjdzr;
 
+import com.isa.jjdzr.service.LoadDatabase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SkirchWebApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(SkirchWebApplication.class, args);
+        LoadDatabase.loadDatabase();
+
     }
 
 }
